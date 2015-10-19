6f9b1df91b2590512982fae7b30978c9952bc99a
Kevin van der Vleuten <kevin.vd.vleuten@gmail.com>
2015-10-19 21:55:57 +0200

    adding project to github

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSURLConnectionDelegate {
 
+    @IBOutlet weak var lbl_name: UILabel!
+    var data = NSMutableData()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        connectMe()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func connectMe() {
+        let url = NSURL(string: "http://api.randomuser.me/")
+        
+        //let json = "{ \"people\": [{ \"firstName\": \"Paul\", \"lastName\": \"Hudson\", \"isAlive\": true }, { \"firstName\": \"Angela\", \"lastName\": \"Merkel\", \"isAlive\": true }, { \"firstName\": \"George\", \"lastName\": \"Washington\", \"isAlive\": false } ] }";
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            //print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+            
+            
+            if let myJson = (NSString(data: data!, encoding: NSUTF8StringEncoding))!.dataUsingEncoding(NSUTF8StringEncoding) {
+                let json = JSON(data: myJson)
+                print(json)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func startConnection() {
+        // edit Info.plist to add exception to make a HTTP request
+        let path = "http://api.randomuser.me/"
+        let url: NSURL = NSURL(string: path)!
+        let request: NSURLRequest = NSURLRequest(URL: url)
+        let connection: NSURLConnection = NSURLConnection(request: request, delegate: self)!
+        connection.start()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        self.data.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        do {
+            if let jsonResult = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary {
+                print(jsonResult)
+            }
+        } catch {
+            print(error)
+        }
+    }
 }
 
