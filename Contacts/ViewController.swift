4d30b7bb4e30a77acb0d56951b3c9ac90865c84e
Kevin van der Vleuten <kpjmvleu@pc131375.local>
2015-10-19 15:39:28 +0200

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Kevin van der Vleuten on 19/10/15.
+//  Copyright © 2015 Kevin van der Vleuten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
