35c01e45edb8a8c85c79e4278057358407c39f5c
Kevin van der Vleuten <kevin.vd.vleuten@gmail.com>
2015-10-20 20:22:24 +0200

    use parsed JSON on main and added flags

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -8,59 +8,106 @@
 
 import UIKit
 
-class ViewController: UIViewController, NSURLConnectionDelegate {
-
-    @IBOutlet weak var lbl_name: UILabel!
+class ViewController: UIViewController {
+    
     var data = NSMutableData()
     
+    @IBOutlet weak var lbl_name: UILabel!
+    @IBOutlet weak var btn_email: UIButton!
+    @IBOutlet weak var img_portrait: UIImageView!
+    @IBOutlet weak var img_flag: UIImageView!
+    @IBOutlet weak var lbl_state_city: UILabel!
+    @IBOutlet weak var lbl_street: UILabel!
+    @IBOutlet weak var lbl_bsn: UILabel!
+    
+    @IBOutlet weak var btn_newPerson: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         connectMe()
     }
-
+    
+    @IBAction func btn_newPerson_Pressed(sender: AnyObject) {
+        connectMe()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     func connectMe() {
         let url = NSURL(string: "http://api.randomuser.me/")
-        
-        //let json = "{ \"people\": [{ \"firstName\": \"Paul\", \"lastName\": \"Hudson\", \"isAlive\": true }, { \"firstName\": \"Angela\", \"lastName\": \"Merkel\", \"isAlive\": true }, { \"firstName\": \"George\", \"lastName\": \"Washington\", \"isAlive\": false } ] }";
-        
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
-            //print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+            (data, response, error) in
             
+            //print(NSString(data: data!, encoding: NSUTF8StringEncoding))
             
             if let myJson = (NSString(data: data!, encoding: NSUTF8StringEncoding))!.dataUsingEncoding(NSUTF8StringEncoding) {
                 let json = JSON(data: myJson)
                 print(json)
+                
+                if let arr = json["results"].array {
+                    let fir = arr[0]["user"]["dob"]
+                    print(fir.stringValue)
+                    
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        var output = (arr[0]["user"]["name"]["title"].stringValue) + " "
+                        output += (arr[0]["user"]["name"]["first"].stringValue) + " "
+                        output += (arr[0]["user"]["name"]["last"].stringValue) + " ("
+                        output += (arr[0]["user"]["gender"].stringValue) + ")"
+                        self.lbl_name.text = output
+                        
+                        output = (arr[0]["user"]["email"].stringValue)
+                        self.btn_email.setTitle(output, forState: .Normal)
+                        
+                        let url = NSURL(string: (arr[0]["user"]["picture"]["medium"].stringValue))
+                        self.downloadImage(url!)
+                        
+                        if let flag = json["nationality"].string {
+                            if (UIImage(named: flag) == nil) {
+                                self.img_flag.image = UIImage(named: "unknown_flag")
+                            }
+                            else {
+                                self.img_flag.image = UIImage(named: flag)
+                            }
+                        }
+                        output = (arr[0]["user"]["location"]["street"].stringValue)
+                        self.lbl_street.text = output
+                        
+                        output = (arr[0]["user"]["location"]["state"].stringValue) + ", " + (arr[0]["user"]["location"]["city"].stringValue)
+                        self.lbl_state_city.text = output
+                        
+                        output = (arr[0]["user"]["phone"].stringValue)
+                        self.lbl_bsn.text = "Tel: " + output
+                    })
+                }
             }
         }
-        
         task.resume()
     }
+
     
-    func startConnection() {
-        // edit Info.plist to add exception to make a HTTP request
-        let path = "http://api.randomuser.me/"
-        let url: NSURL = NSURL(string: path)!
-        let request: NSURLRequest = NSURLRequest(URL: url)
-        let connection: NSURLConnection = NSURLConnection(request: request, delegate: self)!
-        connection.start()
+    func getDataFromUrl(urL:NSURL, completion: ((data: NSData?) -> Void)) {
+        NSURLSession.sharedSession().dataTaskWithURL(urL) { (data, response, error) in
+            completion(data: NSData(data: data!))
+            }.resume()
     }
     
-    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
-        self.data.appendData(data)
-    }
-    
-    func connectionDidFinishLoading(connection: NSURLConnection!) {
-        do {
-            if let jsonResult = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary {
-                print(jsonResult)
+    func downloadImage(url:NSURL){
+        getDataFromUrl(url) { data in
+            dispatch_async(dispatch_get_main_queue()) {
+                self.img_portrait.image = UIImage(data: data!)
             }
-        } catch {
-            print(error)
+        }
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        if UIDevice.currentDevice().orientation.isLandscape.boolValue {
+            print("Landscape")
+            // need to change views on orientation once I found out how that works flawlessly....
+        }
+        else {
+            print("Portrait")
         }
     }
 }
